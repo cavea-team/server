90ae55367f1118bad7886fc3d71ef44b347af062
Nirali Gandhi <niralibitto01@gmail.com>
2020-04-12 17:38:38 +0530

    added Like, Tag, Post and Comment models, Correctly this time

--- src/db/models/comment.ts
+++ src/db/models/comment.ts
@@ -3,36 +3,35 @@ import { createStore } from '../../utils';
 
 const sequelize = createStore();
 
-export class Comment extends Model{
-    public id!: number;
-    public comment!: string;
+export class Comment extends Model {
+  public id!: number;
+  public comment!: string;
 
-    public readonly createdAt!: Date;
-    public readonly updatedAt!: Date;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
 }
 
 Comment.init(
-    {
-        id:{
-            type: DataTypes.INTEGER.UNSIGNED,
-            autoIncrement: true,
-            primaryKey: true,
-            allowNull: false
-        },
-        comment:{
-            type: DataTypes.STRING,
-            allowNull:false
-        }
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+      allowNull: false
     },
-    {
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+  {
     tableName: 'Comment',
     sequelize: sequelize
-    }
+  }
 );
 
 export type CommentModelStatic = typeof Model & {
-    new (values?: object, options?: BuildOptions): Comment;
-  };
-  
-  export default Comment as CommentModelStatic;
-  
+  new (values?: object, options?: BuildOptions): Comment;
+};
+
+export default Comment as CommentModelStatic;
--- src/db/models/like.ts
+++ src/db/models/like.ts
@@ -3,32 +3,30 @@ import { createStore } from '../../utils';
 
 const sequelize = createStore();
 
-export class Like extends Model{
-    public id!: number;
+export class Like extends Model {
+  public id!: number;
 
-    public readonly createdAt!: Date;
-    public readonly updatedAt!: Date;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
 }
 
 Like.init(
-    {   
-        id:
-        {
-            type: DataTypes.INTEGER.UNSIGNED,
-            autoIncrement: true,
-            allowNull: false,
-            primaryKey: true
-        }
-    },
-    {
-        tableName: 'Like',
-        sequelize: sequelize
+  {
+    id: {
+      type: DataTypes.INTEGER.UNSIGNED,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true
     }
+  },
+  {
+    tableName: 'Like',
+    sequelize: sequelize
+  }
 );
 
 export type LikeModelStatic = typeof Model & {
-    new (values?: object, options?: BuildOptions): Like;
-  };
-  
-  export default Like as LikeModelStatic;
-  
+  new (values?: object, options?: BuildOptions): Like;
+};
+
+export default Like as LikeModelStatic;
